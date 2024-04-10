e3bef5955ce8f27b3579c1c4cb5dc19c9c1c083d
Iryna Lyakhova <ippk0629@gmail.com>
2024-04-10 15:07:11 +0300

    Third version

--- task.sql
+++ task.sql
@@ -1,14 +1,16 @@
-CREATE DATABASE ShopDB;
+CREATE DATABASE IF NOT EXISTS ShopDB;
 USE ShopDB;
 
+DROP TABLE IF EXISTS ProductInventory, Warehouses, Products, Countries;
+
 CREATE TABLE Countries (
-    ID INT,
+    ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
 
 CREATE TABLE Warehouses (
-    ID INT,
+    ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     Address VARCHAR(50),
     CountryID INT,
@@ -16,22 +18,30 @@ CREATE TABLE Warehouses (
     FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION
 );
 
+CREATE TABLE Products (
+    ID INT AUTO_INCREMENT,
+    Name VARCHAR(50),
+    PRIMARY KEY (ID)
+);
+
 CREATE TABLE ProductInventory (
-    ID INT,
-    ProductName VARCHAR(50),
+    ID INT AUTO_INCREMENT,
+    ProductID INT,
     WarehouseAmount INT,
     WarehouseID INT,
     PRIMARY KEY (ID),
-    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION
+    FOREIGN KEY (ProductID) REFERENCES Products(ID),
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID)
 );
 
 INSERT INTO Countries (ID, Name)
     VALUES (1, 'Country1'), (2, 'Country2');
-    
+
 INSERT INTO Warehouses (ID, Name, Address, CountryID)
-    VALUES (1, 'Warehouse-1', 'City-1, Street-1', 1),
-           (2, 'Warehouse-2', 'City-2, Street-2', 2);
-           
-INSERT INTO ProductInventory (ID, ProductName, WarehouseAmount, WarehouseID)
-    VALUES (1, 'AwesomeProduct', 2, 1),
-           (2, 'AwesomeProduct', 5, 2);
+    VALUES (1, 'Warehouse-1', 'City-1, Street-1', 1), (2, 'Warehouse-2', 'City-2, Street-2', 2);
+
+INSERT INTO Products (ID, Name)
+    VALUES (1, 'AwersomeProduct'), (2, 'AwersomeProduct');
+
+INSERT INTO ProductInventory (ID, ProductID, WarehouseAmount, WarehouseID)
+    VALUES (1, 1, 2, 1), (2, 1, 5, 2);
